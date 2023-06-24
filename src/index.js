2a76893822198c9e2ebaad1a3b493308244e4975
Vanessa Soares <vanis.sbs@gmail.com>
2023-06-24 10:27:45 -0300

    Apostila03(React Intro): Exercício componentes-JSX
        - Atualizado arquivo README.md

--- src/index.js
+++ src/index.js
@@ -1,17 +1,56 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Med1 from './images/med1.jpg'
+import './Cartao.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const App = ()=>{
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    const estilo1 = () => {
+        return{
+            margin: 'auto',
+            width: '95%',
+            backgroundColor: '#EEE',
+            padding: 12,
+            borderRadius: 8,
+            border: '0.5px solid',
+            fontWeight: 'bold',
+            textAlign: 'center'
+        }
+    }
+
+    const nomeMedicos = {
+        med1: 'José da Silva',
+        med2: 'Maria da Silva',
+        med3: 'Jaqueline Mendes'
+    }
+
+
+    return (
+            <div style={estilo1()}>
+                <p>Profissionais de saúde</p>
+                {/* estilo 2*/}
+                <div  style={{margin: 'auto', width: '100%', backgroundColor: '#EEE', padding: 12, borderRadius: 8,
+                border: '0.5px solid gray', boxSizing:'border-box'}}>
+                    <div class="cartao" >
+                        <img  src={Med1} />
+                        <p>{nomeMedicos.med1}</p>
+                    </div>
+                        
+                    <div class="cartao" >
+                        <img  src={process.env.PUBLIC_URL + "/med2.jpg"} />
+                        <p>{nomeMedicos.med2}</p>
+                    </div>
+        
+                    <div class="cartao" >
+                        <img  src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSoFmmbyHYyGBDBhvJ9hhkfU2VKs250OFjIPH_QL0eiNyDmp3gFsJgvfpZ_ehgTE6oYqEI&usqp=CAU"} />
+                        <p>{nomeMedicos.med3}</p>
+                    </div>                       
+                </div>
+            </div>
+    )
+}
+
+ReactDOM.render(
+    <App/>,
+    document.querySelector("#root")
+)
